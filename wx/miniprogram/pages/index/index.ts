351b6c5628c1e849da79ec4df444e6aa39de071e
zhangxinling2 <2350486903@qq.com>
2023-12-19 08:06:35 +0800

    地图页显示实现，实现定位当前位置

--- wx/miniprogram/pages/index/index.ts
+++ wx/miniprogram/pages/index/index.ts
@@ -3,75 +3,62 @@
 const app = getApp<IAppOption>()
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 
-Component({
-  data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
+Page({
+  data:{
+    setting:{
+        skew:0,
+        rotate:0,
+        showLocation:true,
+        showScale:true,
+        subKey:'',
+        layerStyle:-1,
+        enableZoon:true,
+        enableScroll:true,
+        enableRotate:false,
+        showCompass:false,
+        enable3D:false,
+        enableOverlooking:false,
+        enableSatellite:false,
+        enableTraffic:false,
     },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    location:{
+        latitude:31,
+        longitude:120,
+    },
+    scal:10,
+    markers:[{
+        iconPath:"/resources/car.png",
+        id:0,
+        latitude:23.09994,
+        longitude:113.324520,
+        width:50,
+        height:50,
+    },{
+        iconPath:"/resources/car.png",
+        id:1,
+        latitude:23.09994,
+        longitude:114.324520,
+        width:50,
+        height:50,
+    }],
   },
-  methods: {
-    // 事件处理函数
-    bindViewTap() {
-      // wx.navigateTo({
-      //   url: '../logs/logs',
-      // })
-      console.log('tap')
-    },
-    onChooseAvatar(e: any) {
-      const { avatarUrl } = e.detail
-      const { nickName } = this.data.userInfo
-      this.setData({
-        "userInfo.avatarUrl": avatarUrl,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
-    },
-    onLoad(){
-
-    },
-    onInputChange(e: any) {
-      const nickName = e.detail.value
-      const { avatarUrl } = this.data.userInfo
-      this.setData({
-        "userInfo.nickName": nickName,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
-    },
-    getUserProfile() {
-      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-      wx.getUserProfile({
-        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-        success: (res) => {
-          console.log(res)
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    },
-    updataMoto(){
-      let shouldStop=false
-      setTimeout(() => {
-         shouldStop=true
-       }, 10000);
-      let count=0
-      const update=()=>{
-        count++
-        if(!shouldStop){
-          this.setData({
-            motto: `update count: ${count}`,
+  onMyLocationTap(){
+    wx.getLocation({
+      type:'gcj02',
+      success:res=>{
+        this.setData({
+          location:{
+            latitude:res.latitude,
+            longitude:res.longitude,
           },
-            () => {
-              update()
-            })
-        }
+        })
+      },
+      fail:()=>{
+        wx.showToast({
+          icon:'none',
+          title:'请前往设置页设置',
+        })
       }
-      update()
-    },
-  },
+    })
+  }
 })
