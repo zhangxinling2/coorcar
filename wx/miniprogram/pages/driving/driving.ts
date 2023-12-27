b387a26458f440c2b912349a474ebc414f043c0b
zhangxinling2 <2350486903@qq.com>
2023-12-27 21:22:51 +0800

    driving页面时间和金钱滚动

--- wx/miniprogram/pages/driving/driving.ts
+++ wx/miniprogram/pages/driving/driving.ts
@@ -1,20 +1,21 @@
 // pages/driving/driving.ts
+const centPerSec=0.7
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    elapsed:'15:11:11',
-    fee:'15',
+    elapsed:'00:00:00',
+    fee:'00',
     location:{
       latitude:31,
       longitude:120,
-  },
-  scal:12,
+    },
+    scal:12,
   
   },
-  setUpLocationUpdate(){
+  setupLocationUpdate(){
     wx.startLocationUpdate({
       fail:console.error,
     })
@@ -27,11 +28,38 @@ Page({
       })
     })
   },
+  formatDuration(sec:number){
+    const padString=(n:number)=>{
+       return n<10?'0'+n.toFixed(0):n.toFixed(0)
+    }
+    const h=Math.floor(sec/3600)
+    sec=sec-3600*h
+    const m=Math.floor(sec/60)
+    sec=sec-m*60
+    const s=Math.floor(sec)
+    return `${padString(h)}:${padString(m)}:${padString(s)}`
+  },
+  formatFee(cents:number){
+    return (cents/100).toFixed(2)
+  },
+  setupTimer(){
+    let elapsedSec=0
+    let cent=0
+    setInterval(()=>{
+      elapsedSec++
+      cent++
+      this.setData({
+        elapsed:this.formatDuration(elapsedSec),
+        fee:this.formatFee(cent)
+      })
+    },1000)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-    this.setUpLocationUpdate()
+    this.setupLocationUpdate()
+    this.setupTimer()
   },
 
   /**
